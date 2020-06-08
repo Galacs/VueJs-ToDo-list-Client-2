24dac71e33c2d3a0663139b39eeef7995820f16b
Rémi Ait-Younes <remi.aityounes@gmail.com>
2020-06-08 13:39:10 +0200

    cleared all

---
+++ src/components/todoList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+      <h2>Salut les amis</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todoList',
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
